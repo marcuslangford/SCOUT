e36da895ebbe8fb5f246a40410e82c1f814bd33f
marcuslangford <up777600@myport.ac.uk>
2017-02-08 15:41:40 +0000

    Update and rename server.js to scout.js

---
+++ webpages/scout.js
@@ -0,0 +1,15 @@
+
+var users = ['marcus'];
+var passwords = ['up'];
+
+document.getElementById('login').onclick = function(){
+    var user = document.getElementById('username');
+    var pass = document.getElementById('password');
+    
+    if(user.value == users[0] && pass == passwords[0]){
+        
+}
+
+
+
+
