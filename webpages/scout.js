6ccf878c8a849a47a93618f5a704c064b23308e0
puxtedma10 <puxtedma10@gmail.com>
2017-02-15 14:18:09 +0000

    Add files via upload

--- webpages/scout.js
+++ webpages/scout.js
@@ -1,16 +1,12 @@
 
-var users = ['marcus'];
-var passwords = ['up'];
+//var users = ['marcus'];
+//var passwords = ['up'];
 
-document.getElementById('login').onclick = function(){
-    var user = document.getElementById('username');
-    var pass = document.getElementById('password');
-    
-    if(user.value == users[0] && pass == passwords[0]){
-        alert("well done");
-        
-}
+//document.getElementById('login').onclick = function(){
+//    var user = document.getElementById('username');
+//    var pass = document.getElementById('password');
 
+//    if(user.value == users[0] && pass == passwords[0]){
+//        alert("well done");
 
-
-
+//}
