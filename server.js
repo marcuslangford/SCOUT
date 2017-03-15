bb930dfae23c5c49031121ecf340ac2c45d6793d
mfhamill <mfhamill@hotmail.com>
2017-03-15 15:46:37 +0000

    server.js changes

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
+
 var express = require('express');
 var app = express();
 var path = require('path');
+var mysql = require('mysql');
+
+var config = require('./sql_config.json');
+var sql = mysql.createConnection(config.mysql);
+
 
 // Define the port to run on
-app.set('port', 3000);
+app.set('port', 8080);
+
 
 app.use('/', express.static('webpages', { extensions: ['html'] }));
+
 
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
-  console.log('Server running on port' + port);
+  console.log('Server running on port ' + port);
 });
+
+
+
+ 
